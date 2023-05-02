fbeed483bd836e690c05238194daca9383601294
JerryDcodemaster <ekeminijoseph83@gmail.com>
2023-05-02 12:04:33 +0100

    Learn JS_Cardio

---
+++ app.js
@@ -0,0 +1,27 @@
+// CHALLENGE 1: REVERSE A STRING
+// Return a string in reverse
+// ex. reverseString('hello') ='olleh'
+
+function reverseString(str) {
+    return 'Let'
+}
+
+// CHALLENGE 2: VALIDATE A PALINDROME
+// Return true if palindrome and false if not
+// ex. reverseString('racecar') ='true', isPalindrome('hello') === false
+
+function isPalindrome(str) {
+    return 'Let'
+}
+
+// CHALLENGE 3: REVERSE AN INTEGER
+// Return an integer in reverse
+// ex. reverseInt('521') ='125'
+
+function reverseInt(str) {}
+
+// CHALLENGE 3: REVERSE AN INTEGER
+// Return an integer in reverse
+// ex. reverseInt('521') ='125'
+
+function reverseInt(str) {}
